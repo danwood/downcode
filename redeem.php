5e8bed26f0cdc85a8d5c9f6e69e4358ce1b0e725
Dan Wood <danwood@users.noreply.github.com>
2017-12-21 15:34:06 -0800

    basics starting out

---
+++ redeem.php
@@ -0,0 +1,11 @@
+<?php
+$iOS = false;       // or set to a non-false text value
+if (preg_match("/(\\(iPod|\\(iPhone|\\(iPad)/", $_SERVER['HTTP_USER_AGENT'], $matches)) {
+    $iOS = substr($matches[1], 1);
+}
+$code = isset($_POST['code']) ? htmlspecialchars($_POST['code']) : '';
+$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+
+echo "<h1>submit code</h1><p>$code with email address: $email</p>";
+
+?>
